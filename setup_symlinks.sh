3eed8bd1801411ce50156223596be7c4c0265e9c
Mike Krieger <mikeyk@instagram.com>
2012-08-13 17:28:33 -0700

    updated setup

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -ns $DIR/zshrc ~/.zshrc
 ln -ns $DIR/zsh ~/.zsh
 ln -ns $DIR/vimrc ~/.vimrc
-ln -ns $DIR/vim ~/.vim
+ln -nsf $DIR/vimstuff ~/.vim
 ln -ns $DIR/zsh/themes/prose ~/.oh-my-zsh/themes/prose.zsh-theme
